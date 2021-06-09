f5b4bf7fd9d29d9007af268ee2d25470f6126767
Henry Han <potatoeater0127@gmail.com>
2021-06-09 17:22:46 +0800

    [feat] turn input of Search into a controlled input

--- src/components/Search.component.jsx
+++ src/components/Search.component.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Search() {
+  const [searchValue, setSearchValue] = useState("");
+
+
   return (
     <div>
       <div>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+        />
       </div>
       <div>
         <input type="submit" value="Search" />
